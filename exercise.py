366dc8cf07aedc302fea9929922429d9cd82d3d7
kotarott <kg221kg@gmail.com>
2021-09-03 16:46:45 +0900

    update twitter api

--- exercise.py
+++ exercise.py
@@ -1,15 +1,32 @@
 import tweepy
 from secrets_info import secret
 
-consumer_key = secret.consumer_key
-consumer_secret = secret.consumer_secret
-access_token = secret.access_token
-access_token_secret = secret.access_token_secret
+account = {
+    "consumer_key": secret.consumer_key,
+    "consumer_secret": secret.consumer_secret,
+    "access_token": secret.access_token,
+    "access_token_secret": secret.access_token_secret
+}
 
-# Twitterオブジェクトの生成
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth)
 
-# tweetを投稿
-api.update_status("APIツイートのテスト")
+class Twitter_api:
+    def __init__(self, account):
+        self.consumer_key = account["consumer_key"]
+        self.consumer_secret = account["consumer_secret"]
+        self.access_token = account["access_token"]
+        self.access_token_secret = account["access_token_secret"]
+        self.auth_account()
+
+    def auth_account(self):
+        # Twitterオブジェクトの生成
+        auth = tweepy.OAuthHandler(self.consumer_key, self.consumer_secret)
+        auth.set_access_token(self.access_token, self.access_token_secret)
+        self.api = tweepy.API(auth)
+
+    def tweet(self, text):
+        # tweetする
+        self.api.update_status(text)
+
+if __name__ == "__main__":
+    my_twitter = Twitter_api(account)
+    # my_twitter.tweet("お疲れ様です。")
