af0108b28013360f1c9f49b5401ad18fa385280c
kotarott <kg221kg@gmail.com>
2021-09-19 18:39:36 +0900

    update

--- view.py
+++ view.py
@@ -45,4 +45,4 @@ desktop.start(app_name,end_point,size)
 #desktop.start(size=size,appName=app_name,endPoint=end_point)
 
 # if __name__ == "__main__":
-#     df = get_news_title("ro", "欧州市場サマリー")
+#     pass
